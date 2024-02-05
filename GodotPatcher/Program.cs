0d3039a34c28b2cafbd497422a36749a3e5ec795
Bronuh <bronyx@yandex.ru>
2024-02-05 16:32:59 +0300

    Add --normalize arg for automatic output processing

--- GodotPatcher/Program.cs
+++ GodotPatcher/Program.cs
@@ -14,10 +14,64 @@ class Program
             return;
         }
         
+        if (args.Contains("--normalize"))
+        {
+            Normalize();
+            return;
+        }
+        
         Painter.PaintEverything("./");
         Patcher.DoPatch("./");
     }
 
+    private static void Normalize()
+    {
+        var config = Config.Get().Patcher;
+        Dictionary<string, string> renamings = new()
+        {
+            {"godot.windows.editor.double.x86_64.mono", config.GodotRename},
+            {"godot.windows.template_debug.double.x86_64.mono", "windows_debug_x86_64"},
+            {"godot.windows.template_release.double.x86_64.mono", "windows_release_x86_64"}
+        };
+        
+        string basePath = "./bin/";
+        string buildName = $"{config.Major}.{config.Minor}.{config.Patch}.{config.Status}.mono";
+        File.Create(Path.Combine(basePath, "_sc_")).Dispose();
+        string exportPath = Path.Combine(basePath, @"editor_data\export_templates\"+buildName);
+        
+        Directory.CreateDirectory(exportPath);
+        Directory.Delete(exportPath, true);
+        Directory.CreateDirectory(exportPath);
+
+        var files = Directory.GetFiles(basePath);
+        
+        foreach (var (key, value) in renamings)
+        {
+            foreach (var file in files)
+            {
+                var newFile = file.Replace(key, value);
+                if (file == newFile) continue;
+                
+                if(File.Exists(newFile))
+                {
+                    File.Delete(newFile);
+                }
+
+                var fileName = Path.GetFileName(newFile);
+                if (fileName.Contains("windows_debug_x86_64") || fileName.Contains("windows_release_x86_64"))
+                {
+                    File.Move(file, Path.Combine(exportPath, fileName));
+                }
+                else
+                {
+                    File.Move(file, newFile);
+                }
+            }
+        }
+        
+        
+    }
+    
     private static void Reset()
     {
         try
